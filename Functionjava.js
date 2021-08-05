7cd7fe9772b6a11df86ad80d31c1818a6379a47d
farhanacsebd <farhana.cse.bd@gmail.com>
2021-08-06 03:35:26 +0600

    function

---
+++ Functionjava.js
@@ -0,0 +1,9 @@
+function number(num1, num2) {
+    console.log(num1 + num2)
+    var total = num1 + num2;
+    return total;
+}
+var num1 = 34;
+var num2 = 45;
+var fullResult = num1 + num2;
+console.log('number', fullResult)
